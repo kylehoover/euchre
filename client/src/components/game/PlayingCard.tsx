828659f31eb0bd4b76ab34aea2c77bfed335a70a
Kyle Hoover <khoov22@gmail.com>
2021-02-27 00:03:52 -0600

    Add diamond and heart icons

--- client/src/components/game/PlayingCard.tsx
+++ client/src/components/game/PlayingCard.tsx
@@ -1,17 +1,31 @@
 import classNames from "classnames";
-import { Card } from "../../types";
+import { Card, CardSuit } from "../../types";
+import { ReactComponent as Diamond } from "../../assets/diamond.svg";
+import { ReactComponent as Heart } from "../../assets/heart.svg";
 import "./PlayingCard.scss";
 
 interface Props {
   card: Card;
 }
 
+function getIcon(suit: CardSuit) {
+  switch (suit) {
+    case CardSuit.Diamonds:
+      return <Diamond />;
+    case CardSuit.Hearts:
+      return <Heart />;
+  }
+}
+
 export function PlayingCard(props: Props) {
-  const { card } = props;
+  const { suit, value } = props.card;
 
   return (
-    <div className={classNames("Card", card.suit)}>
-      {card.value} {card.suit}
+    <div className={classNames("Card", suit)}>
+      <div className="signature">
+        <div>{value}</div>
+        {getIcon(suit)}
+      </div>
     </div>
   );
 }
