971c373b22cc20727e06b19021e6c925166d2a73
Kyle Hoover <khoov22@gmail.com>
2021-03-06 00:14:33 -0600

    Add more animations

--- client/src/components/game/TrumpPicker.tsx
+++ client/src/components/game/TrumpPicker.tsx
@@ -1,4 +1,6 @@
-import { useEffect, useState } from "react";
+import { Button } from "@material-ui/core";
+import { animated, useSpring } from "react-spring";
+import { useState } from "react";
 import { PlayingCard } from "./PlayingCard";
 import { delay } from "../../helpers";
 import { useAppSelector } from "../../store";
@@ -12,18 +14,27 @@ export function TrumpPicker() {
     return rounds[rounds.length - 1].trumpCardFromDeck;
   });
 
-  useEffect(() => {
-    async function run() {
+  const actionsProps = useSpring({
+    to: async (next: any) => {
       await delay(100);
       setIsFlipped(false);
-    }
-
-    run();
-  }, []);
+      await delay(500);
+      await next({ opacity: 1, padding: 16, width: 100 });
+    },
+    from: { opacity: 0, padding: 0, width: 0 },
+  });
 
   return (
     <div className="TrumpPicker">
       <PlayingCard card={card} flipped={isFlipped} />
+      <animated.div className="actions" style={actionsProps}>
+        <Button color="primary" variant="contained">
+          Pick up
+        </Button>
+        <Button color="secondary" variant="contained">
+          Pass
+        </Button>
+      </animated.div>
     </div>
   );
 }
